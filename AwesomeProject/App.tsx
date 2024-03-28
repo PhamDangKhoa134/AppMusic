7d73f283241982340f35fbb8bc0f511b3830292d
Pham Khoa <phamdangkhoa134@gmail.com>
2024-03-28 21:14:08 +0700

    update

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,118 +1,79 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, View, Button, TextInput, Alert} from 'react-native';
+import axios from 'axios';
+import React, { useState } from 'react';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [inputName, onChangeInputName] = React.useState('');
+  const [inpuDOB, onChangeInputDoB] = React.useState('');
+  const [inpuCode, onChangeInputCode] = React.useState('');
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <TextInput style = {styles.input} onChangeText={onChangeInputName} value={inputName} placeholder='Nhap ten ban vao dayyyyyy' />
+      <Button title="Add Student" onPress={addStudent} />
+      <Button title="Update Student" onPress={updateStudent} />
+      <Button title="Delete Student" onPress={deleteStudent} />
     </View>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    padding: 5,
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+
 });
 
+const updateStudent = async () => {
+  try {
+      const response = await axios.put('http://demo-api.stecom.vn:8888/api/student/update', {
+        "name": "PCCCC",
+        "dateOfBirth": "2024-03-26T09:37:18.901Z",
+        "studentCode": "232132312",
+        "studentId": 113,
+      });
+      console.log(response.data);
+      // Thêm mã logic xử lý sau khi thêm thành công
+  } catch (error) {
+      console.error('Error adding student:', error);
+      // Xử lý lỗi nếu có
+  }
+};
+
+const addStudent = async () => {
+  try {
+      const response = await axios.post('http://demo-api.stecom.vn:8888/api/student/create-student', {
+        "name": "Khoa dz",
+        "dateOfBirth": "2024-03-26T09:37:18.901Z",
+        "studentCode": "2321323134",
+      });
+      console.log(response.data);
+      // Thêm mã logic xử lý sau khi thêm thành công
+  } catch (error) {
+      console.error('Error adding student:', error);
+      // Xử lý lỗi nếu có
+  }
+};
+
+const deleteStudent = async () => {
+  try {
+      const response = await axios.delete('http://demo-api.stecom.vn:8888/api/student/delete/121', {
+      });
+      console.log(response.data);
+      // Thêm mã logic xử lý sau khi thêm thành công
+  } catch (error) {
+      console.error('Error adding student:', error);
+      // Xử lý lỗi nếu có
+  }
+};
+
 export default App;
+
